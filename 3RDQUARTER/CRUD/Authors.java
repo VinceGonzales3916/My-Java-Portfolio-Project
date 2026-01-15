d6431b5119621a8956e874bba9f79b3e5e92662c
Vince Gonzales <gonzalesrennielvincefajutina@gmail.com>
2026-01-16 00:16:41 +0800

    Authors.java (Sub Class)

---
+++ 3RDQUARTER/CRUD/Authors.java
@@ -0,0 +1,17 @@
+package MyLibrarySystem;
+
+public class Authors {
+    private String authors;
+
+    public Authors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+}
