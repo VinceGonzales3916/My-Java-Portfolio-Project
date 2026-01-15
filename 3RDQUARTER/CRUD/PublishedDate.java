a18e011887def5d8fc94b6a94d412569beb0fee8
Vince Gonzales <gonzalesrennielvincefajutina@gmail.com>
2026-01-16 00:17:53 +0800

    PublishedDate.java (Sub Class)

---
+++ 3RDQUARTER/CRUD/PublishedDate.java
@@ -0,0 +1,17 @@
+package MyLibrarySystem;
+
+public class PublishedDate {
+    private String date;
+
+    public PublishedDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
