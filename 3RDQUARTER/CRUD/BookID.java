ebcb5a59a224a60016eca327c94f8e145a87dbfc
Vince Gonzales <gonzalesrennielvincefajutina@gmail.com>
2026-01-16 00:04:27 +0800

    BookID.java (Sub Class)

---
+++ 3RDQUARTER/CRUD/BookID.java
@@ -0,0 +1,17 @@
+package MyLibrarySystem;
+
+public class BookID {
+    private String id;
+
+    public BookID(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
